caf64ab962b29c6acbf1b7304f6ddfd139817b5e
rahscripts <rahmantjis@gmail.com>
2025-12-01 23:09:22 +0530

    add idea to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
 <div>
-  hellpnpm install next-auth mongoose bcryptjs
+  I AM CREATING A SOCIAL MEDIA OF GYM ENTHUSIAST'S.
+  A WEB APP WHERE USER CAN POST THERE PHOTO WITH TITLE OR CAPTION TO VISUALIZE THERE GROWTH.
 </div>
   );
 }
